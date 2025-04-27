8ced126cb3feb3765ac6d0be28ec54fbfbfc57ff
karci.meliss@gmail.com <karci.meliss@gmail.com>
2025-04-27 17:46:47 +0300

    dao yu kullanmak için domain katmanında repository package tanımlıyorum. tododaoroomrepository iml yi yaptım. yazdığımız daoyu buraya inject edecek . çünkü dao interface oldupu için direkt erişemeyiz erişmek için impl yi yapıyoruz.

---
+++ app/src/main/java/com/meliskarci/simpletodoapp/domain/repository/ToDoDaoRepositoryImpl.kt
@@ -0,0 +1,32 @@
+package com.meliskarci.simpletodoapp.domain.repository
+
+import com.meliskarci.simpletodoapp.data.local.TodoDao
+import com.meliskarci.simpletodoapp.data.local.TodoEntity
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class ToDoDaoRepositoryImpl @Inject constructor(
+    private val todoDao: TodoDao
+) {
+    suspend fun insertTodo(todo: TodoEntity) {
+        todoDao.insertTodo(todo)
+    }
+
+    fun getTodos(): Flow<List<TodoEntity>> = flow {
+        todoDao.getTodos().collect {
+            emit(it)
+        }
+    }
+
+    suspend fun deleteTodo(id: Int) {
+        todoDao.deleteTodo(id)
+    }
+
+    fun getTodoById(id: Int): Flow<TodoEntity> = flow {
+        todoDao.getTodoById(id).collect {
+            emit(it)
+        }
+    }
+
+}
