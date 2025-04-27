08c8811f6277be1d73887ceeb4bb8ff9c6087d0f
karci.meliss@gmail.com <karci.meliss@gmail.com>
2025-04-27 17:21:56 +0300

    AppModule düzenlendir.

--- app/src/main/java/com/meliskarci/simpletodoapp/di/AppModule.kt
+++ app/src/main/java/com/meliskarci/simpletodoapp/di/AppModule.kt
@@ -1,4 +1,40 @@
 package com.meliskarci.simpletodoapp.di
 
-class AppModule {
+import android.content.Context
+import androidx.room.Room
+import com.meliskarci.simpletodoapp.data.local.ToDoDatabase
+import com.meliskarci.simpletodoapp.data.local.TodoDao
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideDatabase(@ApplicationContext appContext: Context): ToDoDatabase {
+        return Room.databaseBuilder(
+            appContext,
+            ToDoDatabase::class.java,
+            "tododb"
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideTodoDao(database: ToDoDatabase) : TodoDao {
+        return database.todoDao()
+    }
+
+    @Provides
+    @Singleton
+    fun provideTodoDaoRepositoryImpl(todoDao: TodoDao) : TodoDaoRepositoryImpl {
+        return TodoDaoRepositoryImpl(todoDao)
+    }
 }
