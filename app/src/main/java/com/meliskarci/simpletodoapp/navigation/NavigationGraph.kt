18c09642ac6483660b73c0590d4369f543b5d879
karci.meliss@gmail.com <karci.meliss@gmail.com>
2025-04-28 02:48:07 +0300

    navigation package içinde navigationgraph ve screen işlemleri yapıldı

---
+++ app/src/main/java/com/meliskarci/simpletodoapp/navigation/NavigationGraph.kt
@@ -0,0 +1,32 @@
+package com.meliskarci.simpletodoapp.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.meliskarci.simpletodoapp.navigation.Screen.Add
+import com.meliskarci.simpletodoapp.navigation.Screen.Detail
+import com.meliskarci.simpletodoapp.navigation.Screen.Home
+import com.meliskarci.simpletodoapp.presentation.add.AddScreen
+import com.meliskarci.simpletodoapp.presentation.detail.DetailScreen
+import com.meliskarci.simpletodoapp.presentation.home.HomeScreen
+
+@Composable
+fun NavigationGraph() {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = Home,
+    ) {
+        composable<Home> {
+            HomeScreen(navController)
+        }
+        composable<Detail> {
+            DetailScreen(navController)
+        }
+        composable<Add> {
+            AddScreen(navController)
+        }
+    }
+}
---
+++ app/src/main/java/com/meliskarci/simpletodoapp/navigation/Screen.kt
@@ -0,0 +1,17 @@
+package com.meliskarci.simpletodoapp.navigation
+
+import kotlinx.serialization.Serializable
+
+sealed interface Screen {
+
+    @Serializable
+    data object Home : Screen
+
+    @Serializable
+    data class Detail(
+        val id: Int
+    ) : Screen
+
+    @Serializable
+    data object Add : Screen
+}
