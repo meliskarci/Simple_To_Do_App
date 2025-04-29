08b0fc18e0f20a16121df4af5c5f707ce9e4a2b5
karci.meliss@gmail.com <karci.meliss@gmail.com>
2025-04-30 01:20:17 +0300

    uygulama için renkler eklendi

---
+++ app/src/main/java/com/meliskarci/simpletodoapp/ui/theme/AppColor.kt
@@ -0,0 +1,14 @@
+package com.meliskarci.simpletodoapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+
+//Renklerim
+val ArkaPlan = Color(0xFF042940)
+val CardRenk1 = Color(0xFFDBF227)
+val CardRenk2 = Color(0xFF005C53)
+val CardRenk3 = Color(0xFF9FC131)
+val YazıRengi = Color(0xFFD6D58E)
+val FloatingButton = Color(0xFFDAFDBA)
+val ButtonRenk = Color(0xFFD8FFDB)
+val SwitchColor = Color(0xFFD8FFDB)
